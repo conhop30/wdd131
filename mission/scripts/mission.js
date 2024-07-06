0c9cfb164522311a642c2dfcd226065e3b63aac9
conhop30 <conhop30@gmail.com>
2024-07-06 10:19:11 -0600

    Add site plan initial.

--- mission/scripts/mission.js
+++ mission/scripts/mission.js
@@ -1,15 +1,15 @@
-const themeSelector = document.getElement("select");
+const themeSelector = document.getElementById("theme-toggle");
 
 themeSelector.addEventListener("change", changeTheme);
 
 function changeTheme() {
-    if (themeSelector == "dark") {
-        document.getElement("body").removeClass("light");
-        document.getElement("body").addClass("dark");
+    if (themeSelector == "dark-mode") {
+        document.getElement("body").removeClass("light-mode");
+        document.getElement("body").addClass("dark-mode");
     }
     else {
-        document.getElement("body").removeClass("dark");
-        document.getElement("body").addClass("light");
+        document.getElement("body").removeClass("dark-mode");
+        document.getElement("body").addClass("light-mode");
     }
 }
 
